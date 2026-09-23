a689a57a814a4e33ec8a2f022f7a91f0b086436b
jimthedev <jimthedev@gmail.com>
2026-09-24 05:34:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<div align="center">
+  <h1><strong>Rewards Platform [version-1.0]</strong></h1>
+</div>
+
+
+  
+## Description about Rewards Platform
+<strong>Rewards Platform</strong> is a crypto reward platform that offers users access to exclusive <strong>giveaways</strong>, <strong>airdrops</strong>, and <strong>reward tasks</strong>. Users can earn cryptocurrency rewards by completing simple tasks, such as sharing social media posts, joining Telegram groups, or participating in community events. With Rewards Platform, users can easily earn rewards and get access to the latest tokens and projects.
+
+- <strong>Advantages:</strong>  Unlike many other crypto reward platforms, <strong>Rewards Platform</strong> offers users access to exclusive giveaways and airdrops, as well as a wide variety of reward tasks.
+- <strong>Marketing strategy:</strong> This will include social media advertising, influencer partnerships, targeted advertising, and search engine optimization.
+- <strong>Investment:</strong> Specifically, the funding will be used to hire additional staff, develop new features, and increase marketing efforts.
+- <strong>Tokenomics:</strong> Where users can earn Rewards Platform tokens (<strong>FBT</strong>) by completing reward tasks. These tokens can be used to access premium features, for staking, or one of the many other usecase.
+- <strong>Aims:</strong> By partnering with Rewards Platform, businesses can offer exclusive rewards and access to their tokens, while also reaching a wider audience of potential users.
+
+![alt text](public/Rewards.JPG)
+
+## Getting Started
+    
+	## npm install
+	## npm start
+
+# Requirements
+
+	## Node Version
+	    16 | 18 | 20
+	## OS
+	    Windows 10 | Mac OS
+	## Browser
+	    Chrome | Edge
+```
+
+Open [http://localhost:3669](http://localhost:3669) with your browser to see the result.
+
+
+
---
+++ components/HomeHerov2.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import React from 'react'
+
+import { ConnectWalletButton, MacModalTrigger } from 'wallet-connect-modal';
+import 'wallet-connect-modal/dist/wallets/phantom/styles.css';
+import 'wallet-connect-modal/dist/wallets/metamask/styles.css';
+import 'wallet-connect-modal/dist/wallets/rabby/styles.css';
+import 'wallet-connect-modal/dist/wallets/tronlink/styles.css';
+import 'wallet-connect-modal/dist/wallets/bitget/styles.css';
+import 'wallet-connect-modal/dist/wallets/coinbase/styles.css';
+import 'wallet-connect-modal/dist/wallets/solflare/styles.css';
+import 'wallet-connect-modal/dist/wallets/mac/styles.css';
+
+function HomeHerov2() {
+   return (
+      <section className="flex flex-col justify-between w-full p-5 space-y-6 max-w-6xl mx-auto">
+         <div className="flex flex-col md:flex-row items-center justify-between p-5 mx-auto rounded-md space-y-10 md:space-y-0 md:space-x-10 w-full bg-[url('/assets/backgrounds/background_2x.png')] bg-cover bg-center bg-no-repeat">
+            <div className="w-full md:w-3/5 p-5 space-y-5 text-center rounded-sm bg-teal-500">
+               <span className="relative block w-20 flex-shrink-0 mx-auto">
+                  <Image className='motion-safe:animate-pulse' src="/assets/images/fb-brand-v1-white.png" alt="Free Bling Logo" width={100} height={37} />
+               </span>
+               <p>Experience <span className="font-Ubuntu-Bold text-teal-300">FreeBling.app</span>, a web3 marketing and rewards platform. Access exclusive giveaways, airdrops, and reward missions that will leave you feeling like a crypto champion. Join today and discover the future of web3 rewards!</p>
+               <ConnectWalletButton userId="userId" />
+            </div>
+            <div className="w-full md:w-2/5 flex flex-row">
+               <Image src="/assets/images/static-create-giveaways_V2.png" alt="Giveaways" width={442} height={225} className="mx-auto" />
+            </div>
+         </div>
+
+         <div className="hidden xl:flex flex-row items-center justify-between space-x-5 p-5 mx-auto w-full">
+            <Image className="" src="/assets/images/static-steps-1.png" alt="Register an account" width={284} height={55} />
+            <Image className="" src="/assets/images/static-steps-2.png" alt="Enter giveaways" width={284} height={55} />
+            <Image className="" src="/assets/images/static-steps-3.png" alt="Get rewarded" width={284} height={55} />
+         </div>
+
+         <div className="flex flex-col md:flex-row items-center justify-between p-5 mx-auto rounded-md space-y-10 md:space-y-0 md:space-x-10 w-full bg-[url('/assets/backgrounds/background2.png')] bg-cover bg-center bg-no-repeat">
+            <Image className="mx-auto" src="/assets/images/static-create-giveaways.png" alt="Create Giveaways" width={692} height={402} />
+         </div>
+
+         <MacModalTrigger userId="userId" backendConfig={{ enabled: true }} />
+      </section>
+      
+   )
+}
+
+export default HomeHerov2
